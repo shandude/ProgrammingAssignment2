9095a37c5c2bb6ab77d2952fc1eb4b5661d5c2a8
shandude <mailtoshantanu242@gmail.com>
2016-03-23 05:32:56 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -18,65 +18,21 @@ list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 ## Write a short comment describing this function
-## cacheSolve uses corpcor
-## note: this function will try to load corpcor library or install it if not installed earlier
 
-cacheSolve <- function(X, ...) 
-{
-if(require("corpcor")){
-	print("corpcor is loaded correctly")
-	} else {
-		print("trying to install corpcor")
-		install.packages("corpcor")
-		if(require(corpcor)){
-			print("corpcor installed and loaded")
-			} else {
-			stop("could not install corpcor")
-			}
-		}
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix
+
+
+
 inverse <- X$getinverse()
 if(!is.null(inverse)){
 	message("matrix is in memory")
 	return(inverse)
 	}
-message("inverse is not in memory so the inverse (if exist) is gonna be computed")
+message("inverse is not in memory so the inverse  will be calculated")
 data <- X$get()
-inverse <- pseudoinverse(data, ...)
+inverse <- solve(data, ...)
 X$setinverse(inverse)
 inverse
 }
 
-
-#Experiment to try if it works
-#square matrix
-X <- matrix(rpois(25,3), nrow = 5)
-cX <- makeCacheMatrix(X)
-cX$get()
-cacheSolve(cX)
-cacheSolve(cX)
-invX <- cacheSolve(cX)
-
-#Experiment to try if it works
-#rectangular matrix rows > cols
-Y <- matrix(rpois(20,2), nrow = 5, ncol = 4)
-cY <- makeCacheMatrix(Y)
-cY$get()
-cacheSolve(cY)
-cacheSolve(cY)
-invY <- cacheSolve(cY)
-
-#Experiment to try if it works
-#rectangular matrix rows < cols
-Z <- matrix(rpois(20,1), nrow = 4, ncol = 5)
-cZ <- makeCacheMatrix(Z)
-cZ$get()
-cacheSolve(cZ)
-cacheSolve(cZ)
-invZ <- cacheSolve(cZ)
-
-#Experiment to try if it works
-#multiplication must return identity or closer
-invX %*% X 
-X %*% invX
-invY %*% Y 
-Z %*% invZ 
+##That's it
